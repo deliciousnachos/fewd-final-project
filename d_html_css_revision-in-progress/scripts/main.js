89f27bf422caba047dc13d0d254401e464c73f98
Nicole Boettcher <11392575+deliciousnachos@users.noreply.github.com>
2018-03-25 19:19:05 -0400

    add view toggle

--- d_html_css_revision-in-progress/scripts/main.js
+++ d_html_css_revision-in-progress/scripts/main.js
@@ -1,1 +1,40 @@
-// alert('main.js is connected');
+// nothing before here 
+$( document ).ready(function() {
+// nothing before here 
+
+
+
+// $('#butt-sm') = $smallButton
+// $('#butt-med') = $mediumButton
+var $largeButton = $('#butt-lg');
+
+function turnRed() {
+  (largeButton).html('wow');
+}
+
+function makeLarge() {
+  $('#butt-lg').on('click', 'turnRed()');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// nothing after here 
+}); 
+// closes document ready
+// nothing after here 
