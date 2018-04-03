792948902a78cb7244979369d59b38ac422c6d53
Nicole Boettcher <11392575+deliciousnachos@users.noreply.github.com>
2018-04-02 20:50:02 -0400

    failing at active state, starting media queries

--- scripts/main.js
+++ scripts/main.js
@@ -14,8 +14,11 @@ var $mediumButton = $('#butt-med');
 var $largeButton = $('#butt-lg');
 
 $largeButton.click(function() {
-  // $largeButton.toggleClass("a.button.active"); // not working
+  // $('a').toggleClass(".button:active"); // not working
   $('.cat-item').css('max-width', '380px');
+  $('.active').not($(this)).removeClass('active');
+    $(this).toggleClass('active');
+    event.stopPropagation();
 });
 
 $mediumButton.click(function() {
